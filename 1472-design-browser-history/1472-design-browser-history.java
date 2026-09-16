663f61187a1fa5ec91c1bb7dda9f01811f35f47b
vtu30316-pyt <vtu30316@veltech.edu.in>
2026-09-16 11:07:58 +0530

    Time: 47 ms (93.64%), Space: 54 MB (27.32%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class BrowserHistory {
+
+    ArrayList<String> history;
+    int current;
+
+    public BrowserHistory(String homepage) {
+        history = new ArrayList<>();
+        history.add(homepage);
+        current = 0;
+    }
+
+    public void visit(String url) {
+        // Remove forward history
+        while (history.size() > current + 1) {
+            history.remove(history.size() - 1);
+        }
+
+        history.add(url);
+        current++;
+    }
+
+    public String back(int steps) {
+        current = Math.max(0, current - steps);
+        return history.get(current);
+    }
+
+    public String forward(int steps) {
+        current = Math.min(history.size() - 1, current + steps);
+        return history.get(current);
+    }
+}
